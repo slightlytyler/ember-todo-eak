21e900c12c1a1aafe8735e43c8c3880ff2b8cfc5
Tyler Martinez <slightlytyler@gmail.com>
2014-06-20 19:24:34 -0500

    remaining and inflection added

--- app/todos/controller.js
+++ app/todos/controller.js
@@ -19,5 +19,12 @@ export default Ember.ArrayController.extend({
       // Save the model
       todo.save();
     }
-  }
+  },
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'todo' : 'todos';
+  }.property('remaining')
 });
