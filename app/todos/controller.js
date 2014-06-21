15345c7a24fef8739e60453e1232df56cb5b9cb2
Tyler Martinez <slightlytyler@gmail.com>
2014-06-20 22:36:13 -0500

    show when all todos are complete

--- app/todos/controller.js
+++ app/todos/controller.js
@@ -37,5 +37,8 @@ export default Ember.ArrayController.extend({
   }.property('completed'),
   completed: function() {
     return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+  allAreDone: function(key, value) {
+    return !!this.get('length') && this.isEvery('isCompleted');
   }.property('@each.isCompleted')
 });
