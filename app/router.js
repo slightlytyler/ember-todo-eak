38fc40a9b8bee6f94d18cb99075045fa99c4fe3d
Tyler Martinez <slightlytyler@gmail.com>
2014-06-20 20:41:16 -0500

    additional child routes

--- app/router.js
+++ app/router.js
@@ -1,7 +1,9 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    //additional child routes here later
+  });
   this.route('component-test');
   this.route('helper-test');
   // this.resource('posts', function() {
